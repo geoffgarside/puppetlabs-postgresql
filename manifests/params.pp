6b880e06bd0f321b53f789a5af987e87ae6427a6
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-15 20:06:29 +0000

    Update $service_rc to use Users path

--- manifests/params.pp
+++ manifests/params.pp
@@ -173,7 +173,12 @@ class postgresql::params inherits postgresql::globals {
       $firewall_supported   = pick($firewall_supported, false)
       $needs_initdb         = pick($needs_initdb, false)
 
-      $service_rc           = "~/Library/LaunchAgents/${service_name}.plist"
+      if $user != 'pgsql' {
+        $service_rc         = "/Users/${user}/Library/LaunchAgents/${service_name}.plist"
+      } else {
+        $service_rc         = "/Library/LaunchDaemons/${service_name}.plist"
+      }
+
       $service_source       = "/usr/local/opt/postgresql/${service_name}.plist"
     }
 
